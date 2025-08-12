c7eea7accb4a7940dfaee70607cee9b6a0e4d9d3
Renato245 <renatocarvalho01234@gmail.com>
2025-08-11 22:44:03 -0300

    feat: Adicionando variaveis de ambiente e empacotando aplicação java no docker

--- src/main/java/com/renatocarvalho/pdf_checker/configs/CorsConfig.java
+++ src/main/java/com/renatocarvalho/pdf_checker/configs/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.renatocarvalho.pdf_checker.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.origins}")
+    private String origins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(origins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
